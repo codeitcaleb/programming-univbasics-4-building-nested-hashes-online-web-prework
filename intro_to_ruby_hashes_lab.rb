b3d19bc741da2d468cc7cbbb0818e3d0e7735b5d
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-10 18:31:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require "pry"
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
@@ -92,6 +93,7 @@ def monopoly_with_fourth_tier
 	      }
 	    }
 	  }
+binding.pry
 end
 
 
